cc5a5f13615bf5f6cd2af58a0d8e9cce652228c6
Ana Mattos <anajuliamattos02@gmail.com>
2026-03-26 21:49:29 -0300

    Permissao para abrir camera do celular

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,81 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
 
-export default function HomeScreen() {
+export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+ const [foto, setFoto] = useState<string | null>(null);
+ 
+  // Isso diz ao TypeScript que a referência é para um componente CameraView
+const cameraRef = useRef<CameraView>(null);
+
+  // 1. Verificação de Permissão
+  if (!permission) return <View />;
+  
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Precisamos da sua permissão para abrir a câmera</Text>
+        <TouchableOpacity onPress={requestPermission} style={styles.button}>
+          <Text style={styles.text}>Conceder Permissão</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  // 2. Função para tirar a foto
+  async function tirarFoto() {
+    if (cameraRef.current) {
+      const options = { quality: 0.5, base64: true };
+    const data = await cameraRef.current.takePictureAsync(options);
+if (data && data.uri) {
+  setFoto(data.uri);
+}
+    }
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      {foto ? (
+        <View style={styles.preview}>
+          <Image source={{ uri: foto }} style={styles.image} />
+          <TouchableOpacity onPress={() => setFoto(null)} style={styles.button}>
+            <Text style={styles.text}>Tirar Outra</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <CameraView style={styles.camera} ref={cameraRef}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.captureButton} onPress={tirarFoto}>
+              <Text style={styles.text}>Tirar Foto</Text>
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: { flex: 1, justifyContent: 'center' },
+  camera: { flex: 1 },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
     flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 64,
+  },
+  captureButton: {
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    gap: 8,
+    backgroundColor: '#fff',
+    padding: 15,
+    borderRadius: 50
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  preview: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  image: { width: '80%', height: '70%', borderRadius: 10 },
+  text: { fontSize: 18, fontWeight: 'bold', color: 'black' },
+  button: { marginTop: 20, backgroundColor: '#ddd', padding: 10 }
 });
